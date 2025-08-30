6aa9b597a98743620b0a415923ba5fa440037f92
shinoa <nabilynwa@gmail.com>
2025-08-30 21:30:49 +0700

    fix image missing

--- src/Page/Landing-Page/component/MerchantList.jsx
+++ src/Page/Landing-Page/component/MerchantList.jsx
@@ -5,8 +5,8 @@ export const MerchantList = () => {
         {
             name: "Atheist Golf Stick ",
             brand: "Titleist",
-            background: "./public/golf-image.png",
-            image: "./public/golf-brand.png",
+            background: "/golf-image.png",
+            image: "/golf-brand.png",
             user: "220+",
             price: "70000",
             desc: "Achieve precision and power with this premium golf stick, crafted for peak performance on every swing, whether you’re on the range or in a championship match."
@@ -14,8 +14,8 @@ export const MerchantList = () => {
         {
             name: "Naik Air Maxx21",
             brand: "Nike",
-            background: "./public/run-image.png",
-            image: "./public/run-brand.png",
+            background: "/run-image.png",
+            image: "/run-brand.png",
             user: "800+",
             price: "120000",
             desc: "Lightweight running shoes with superior cushioning for comfort from start to finish, keeping your stride smooth during sprints, jogs, or daily training."
@@ -23,8 +23,8 @@ export const MerchantList = () => {
         {
             name: "Yanglex Astrox 100 ZZ",
             brand: "Yonex",
-            background: "./public/badminton-image.png",
-            image: "./public/badminton-brand.png",
+            background: "/badminton-image.png",
+            image: "/badminton-brand.png",
             user: "200+",
             price: "80000",
             desc: "High-performance badminton racket offering explosive smashes and precise control, perfect for players aiming to dominate rallies with speed and accuracy."
@@ -32,8 +32,8 @@ export const MerchantList = () => {
         {
             name: "Naik Phantom GX 2",
             brand: "Nike",
-            background: "./public/soccer-image.png",
-            image: "./public/soccer-brand.png",
+            background: "/soccer-image.png",
+            image: "/soccer-brand.png",
             user: "670+",
             price: "120000",
             desc: "Soccer boots engineered for maximum grip, control, and acceleration on every play, helping you outpace opponents and stay in command of the ball."
@@ -75,7 +75,7 @@ export const MerchantList = () => {
                                     {product.brand}
                                 </h1>
                                 <div className="flex">
-                                    <img src='./public/person.png' alt="" />
+                                    <img src='/person.png' alt="" />
                                     <h1 id="cool" className="text-[12px] text-[#FFC800] ml-1">
                                         {product.user}
                                     </h1>
